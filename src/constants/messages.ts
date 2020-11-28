4c22b321b6d98d12e72dd9de57f32f4d665fa359
mauroerta <mauro@vlkstudio.com>
2020-11-28 22:56:25 +0100

    feat: notification mapping

--- src/constants/messages.ts
+++ src/constants/messages.ts
@@ -1,9 +1,24 @@
-import { FetchGoodMorningType, UpdateGoodMorningType } from '../state/goodMornings/types';
-import { Messages } from '../state/notifications';
-
+import {
+  FetchGoodMorningType,
+  UpdateGoodMorningType,
+  CreateGoodMorningType,
+  RemoveGoodMorningType,
+} from "../state/goodMornings";
+import { Messages } from "../state/notifications";
 
 export const messages: Messages = {
-  [FetchGoodMorningType.FAILURE]: 'Ops, qualcosa è andato storto mentre cercavamo di recuperare i tuoi dolcissimi buongiornissimi!',
-  [UpdateGoodMorningType.SUCCESS]: 'Il tuo buongiornissimo è stato modificato, adesso sembra molto meglio!',
-  [UpdateGoodMorningType.FAILURE]: 'Ops, non siamo riusciti ad aggiornare il tuo buongiorno',
+  [FetchGoodMorningType.FAILURE]:
+    "Ops, qualcosa è andato storto mentre cercavamo di recuperare i tuoi dolcissimi buongiornissimi!",
+  [CreateGoodMorningType.SUCCESS]:
+    "Un nuovo buongiornissimo è pronto per essere condiviso col mondo",
+  [CreateGoodMorningType.FAILURE]:
+    "Oh no! Non siamo riusciti a creare il buongiornissimo, sarà forse un complotto dei poteri forti? Riprova più tardi",
+  [UpdateGoodMorningType.SUCCESS]:
+    "Il tuo buongiornissimo è stato modificato, adesso sembra molto meglio!",
+  [UpdateGoodMorningType.FAILURE]:
+    "Ops, non siamo riusciti ad aggiornare il tuo buongiorno",
+  [RemoveGoodMorningType.SUCCESS]:
+    "Il tuo buongiornissimo non rallegrerà più le giornate di nessuno",
+  [RemoveGoodMorningType.FAILURE]:
+    "Ops, non siamo riusciti ad eliminare it tuo buongiornissimo, forse è troppo bello? Riprova più tardi",
 };
